d4f665d97b1f353f09d08e236522410eaa291cec
TiffanyYang411 <suhanyang902@gmail.com>
2025-04-21 01:24:34 +0800

    tw-0421-1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 // src/components/Navbar.jsx
 import { Link, useLocation } from 'react-router-dom';
 import '../styles/Navbar.css'; // 引入 CSS 樣式檔案
+import cartIcon from '../images/cart-icon-logout.svg';
+import userIcon from '../images/user-icon.svg';
 
 function Navbar() {
   const location = useLocation(); // 取得目前路徑
@@ -11,20 +13,32 @@ function Navbar() {
     { name: '我的行程', path: '/my-trip' },
     { name: '關於我們', path: '/about' },
     { name: '常見問題', path: '/faq' },
-    { name: '登入', path: '/login' },
   ];
 
   return (
     <nav className="navbar">
-      <div className="navbar-logo">Élan Journeys</div>
-      <ul className="navbar-links">
-        {navLinks.map((link) => (
-          <li key={link.path} className={location.pathname === link.path ? 'active' : ''}>
-            <Link to={link.path}>{link.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div className="navbar-logo">Élan Journeys</div>
+
+    <ul className="navbar-links">
+      {navLinks.map((link) => (
+        <li
+          key={link.path}
+          className={location.pathname === link.path ? 'active' : ''}
+        >
+          <Link to={link.path}>{link.name}</Link>
+        </li>
+      ))}
+    </ul>
+
+    <div className="navbar-icons">
+      <Link to="/cart" className="icon-link" title="購物車">
+        <img src={cartIcon} alt="Cart Icon" className="nav-icon" />
+      </Link>
+      <Link to="/login" className="icon-link" title="會員登入">
+        <img src={userIcon} alt="User Icon" className="nav-icon" />
+      </Link>
+    </div>
+  </nav>
   );
 }
 
