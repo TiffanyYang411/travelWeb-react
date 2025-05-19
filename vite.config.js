4c47dfae6450e7f73affe74237a6169b143dd704
TiffanyYang411 <suhanyang902@gmail.com>
2025-05-20 02:47:07 +0800

    tw-0520-02:47

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base:'/travelWeb-react/',
-  plugins: [react()],
+  base: '/travelWeb-react/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.',       // 複製到 dist 根目錄
+          rename: '404.html', // 讓 GitHub Pages fallback 到 React App
+        },
+      ],
+    }),
+  ],
 })
+
