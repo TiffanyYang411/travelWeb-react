d7bd22b489ea2f2efe98e179a65efaedcd8b0254
TiffanyYang411 <suhanyang902@gmail.com>
2025-05-23 13:32:43 +0800

    tw-0523-13:32

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,35 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+// export default defineConfig({
+//   base: '/travelWeb-react/',
+//   plugins: [
+//     react(),
+//     viteStaticCopy({
+//       targets: [
+//         {
+//           src: 'dist/index.html',
+//           dest: '.',
+//           rename: '404.html',
+//         },
+//       ],
+//     }),
+//   ],
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: 'index.html',
+//       },
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   base: '/travelWeb-react/',
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'dist/index.html',
-          dest: '.',
-          rename: '404.html',
-        },
-      ],
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      },
-    },
-  },
+  plugins: [react()],
 })
-
 
