3379d564e412447c4e21859374b555584ac2b11b
TiffanyYang411 <suhanyang902@gmail.com>
2025-05-26 17:48:18 +0800

    tw-0526-17:48

--- src/data/whyUs.js
+++ src/data/whyUs.js
@@ -3,18 +3,18 @@
 export const whyUsItems = [
   {
     title: '專屬策劃師',
-    description: `我們相信，每位旅人都值得被用心對待。<br />專屬規劃師將一對一為您服務，深入了解您的喜好與需求，打造專屬於您的獨特旅程。從行程設計、住宿選擇<br />到在地體驗，皆由專業團隊量身規劃`,
+    description: `我們相信，每位旅人都值得被用心對待。<br />專屬規劃師將一對一為您服務，深入了解您的喜好與需求，打造專屬於您的獨特旅程。<br />從行程設計、住宿選擇到在地體驗，<br />皆由專業團隊量身規劃`,
     image: `${import.meta.env.BASE_URL}images/whyus-planner.jpg`,
   },
   {
     title: '高端客製化',
-    description: `拒絕制式行程，只為您打造獨一無二的高端旅行體驗。<br />我們結合國際資源與在地人脈，依據您的品味與興趣精心設計，<br />無論是寧靜的海島假期、藝術之旅或文化深度遊，<br />或是從交通、住宿到私密體驗皆可客製`,
+    description: `拒絕制式行程，只為您打造獨一無二的高端旅行體驗。<br />我們結合國際資源與在地人脈，依據您的品味與興趣<br />精心設計，無論是寧靜的海島假期、<br />藝術之旅或文化深度遊，或是從交通、<br />住宿到私密體驗皆可客製`,
     image1: `${import.meta.env.BASE_URL}images/whyus-custom1.jpg`,
     image2: `${import.meta.env.BASE_URL}images/whyus-custom2.jpg`,
   },
   {
     title: '專業服務',
-    description: `真正的高端服務，體現在每一個看不見的細節。<br />我們擁有經驗豐富的專業團隊，提供全程支援與即時協助，<br />讓您無後顧之憂。從旅前諮詢、行程控管到旅途中<br />任何突發狀況，我們皆即時回應、主動解決`, // ❌不用任何<br />，維持一行
+    description: `真正的高端服務，體現在每一個看不見的細節。我們擁有經驗豐富的專業團隊，<br />提供全程支援與即時協助，讓您無後顧之憂。從旅前諮詢、行程控管到旅途中<br />任何突發狀況，我們皆即時回應、主動解決`, // ❌不用任何<br />，維持一行
     image: `${import.meta.env.BASE_URL}images/whyus-service.jpg`,
   },
 ];
